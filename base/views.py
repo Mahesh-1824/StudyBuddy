b9cb91e0cbc8911d9e16feca21cee5d28c8cfaf5
Mahesh <maheshderangula053@gmail.com>
2024-11-11 12:55:07 +0530

    Worked on Templates and urls. And how they work

--- base/views.py
+++ base/views.py
@@ -3,10 +3,21 @@ from django.http import HttpResponse
 
 # Create your views here.
 
+rooms = [
+    {'id':1, 'name':'Lets learn python!'},
+    {'id':2, 'name':'Design with me'},
+    {'id':3, 'name':'Frontend developers'},
+]
 def home(request):
     # return HttpResponse('Home Page')
-    return render(request, 'home.html')
+    context = {'rooms' : rooms}
+    return render(request, 'base/home.html', context)
 
-def room(request):
+def room(request,pk):
     # return HttpResponse('ROOM')
-    return render(request, 'room.html')
+    room = None
+    for i in rooms:
+        if i['id'] == int(pk):
+            room = i
+    context = {'room' : room}
+    return render(request, 'base/room.html',context)
